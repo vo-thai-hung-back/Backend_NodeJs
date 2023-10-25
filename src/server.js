b59d27886b8f0e1d57159523d2c4c1c1d5a6ee1d
vo-thai-hung-back <vothaihung4777@gmail.com>
2023-10-25 11:30:23 +0700

    usemysql2

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ const express = require('express')
 require('dotenv').config()
 const configViewEngine = require('./config/viewEngine')
 const webRouters = require('./routes/web')
+const mysql = require('mysql2');
 const app = express()
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
+
 
 // config template engine
 
@@ -12,6 +14,23 @@ configViewEngine(app);
 
 // khai báo route
 app.use('/', webRouters);
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '123456',
+    database: 'demo'
+});
+
+// simple query
+connection.query(
+    'select * from Users u ',
+    function (err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
 
 
 app.listen(port, hostname, () => {
