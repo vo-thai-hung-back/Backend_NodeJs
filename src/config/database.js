c4eb66f659cc92275bf3d67d19768bd304e54f66
vo-thai-hung-back <vothaihung4777@gmail.com>
2023-10-25 13:06:41 +0700

    connectpool

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,26 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+// code ở dưới dành cho 1 client
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_POST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+//code ở dưới dành cho đi làm
+
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_POST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    //set up 3 tk dưới là sử dụng đc tk connect pool r
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
-
 
 module.exports = connection;
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,23 @@
+const { json } = require('express');
+const connection = require('../config/database')
+
 const getHomepage = (req, res) => {
-    res.render('sample.ejs')
+
+    let users = [];
+    connection.query(
+        'select * from Users u ',
+        function (err, results, fields) {
+            users = results;
+            console.log(results);
+            //console.log(fields);
+            console.log('check : ', users);
+            res.send(JSON.stringify(users))
+
+            // muốn xuất view là đoạn dưới
+            //res.render('sample.ejs');
+        }
+    );
+
 }
 const getImg = (req, res) => {
     res.send('sample.ejs')
